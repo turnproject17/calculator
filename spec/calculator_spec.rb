0163ee6ce4a9798345ac4cdc665f963e07949521
Brijesh kumar <36134190+turnproject17@users.noreply.github.com>
2025-07-25 19:03:19 +0530

    handle multiple cases for 0

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -4,5 +4,20 @@ Spec.describe Calculator do
       @calculator = Calculator.new
       expect(@calculator.add()).to eq(0)
     end
+
+    it " return 0 if string argument passed" do
+      @calculator = Calculator.new
+      expect(@calculator.add('abc')).to eq(0)
+    end
+
+    it " return 0 if negative numbers passed" do
+      @calculator = Calculator.new
+      expect(@calculator.add(-1)).to eq(0)
+    end
+
+    it " return 0 if number is greater than 1000 passed" do
+      @calculator = Calculator.new
+      expect(@calculator.add(1001)).to eq(0)
+    end
   end
 end
