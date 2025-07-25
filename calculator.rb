ed58b6f39739bd4461565eb35142c3e72dae59dc
Brijesh kumar <36134190+turnproject17@users.noreply.github.com>
2025-07-25 19:21:04 +0530

    added logic to scan out all integers and added validation for negative numbers

--- calculator.rb
+++ calculator.rb
@@ -1,9 +1,25 @@
 class Calculator
   def init
+    @numbers = []
   end
 
   def add(numbers)
+    get_sumable_numbers(numbers)
     validate_number
-    numbers_array = get_sumable_numbers(numbers)
+    return @numbers.sum
+  end
+
+  private
+
+  def validate_number
+    negative_numbers = @numbers.select{|num| num < 0}
+    if negative_numbers.any?
+      raise "negative numbers are not allowed: #{negative_numbers.join(',')}"
+    end
+  end
+
+  def get_sumable_numbers(numbers)
+    @numbers = numbers.scan(/-?\d+/).map(&:to_i)
+                      .select {|num| num <= 1000}
   end
 end
