0b5cbe9917be09cc6d8c916a12645a04a69ea9fc
Brijesh kumar <36134190+turnproject17@users.noreply.github.com>
2025-07-25 19:05:44 +0530

    refactored

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,23 +1,21 @@
 Spec.describe Calculator do
+  let(:calc) { Calculator.new }
+
   describe "add" do // writing all the possible test cases related to add method
     it " return 0 if no argument passed" do
-      @calculator = Calculator.new
-      expect(@calculator.add()).to eq(0)
+      expect(calc.add()).to eq(0)
     end
 
     it " return 0 if string argument passed" do
-      @calculator = Calculator.new
-      expect(@calculator.add('abc')).to eq(0)
+      expect(calc.add('abc')).to eq(0)
     end
 
     it " return 0 if negative numbers passed" do
-      @calculator = Calculator.new
-      expect(@calculator.add(-1)).to eq(0)
+      expect(calc.add(-1)).to raise_error("negative numbers are not allowed: -1")
     end
 
     it " return 0 if number is greater than 1000 passed" do
-      @calculator = Calculator.new
-      expect(@calculator.add(1001)).to eq(0)
+      expect(calc.add(1001)).to eq(0)
     end
   end
 end
