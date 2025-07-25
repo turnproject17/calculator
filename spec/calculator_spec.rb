f3b3d03e2c633ae8b013d6b56858d4bc862d7b1e
Brijesh kumar <36134190+turnproject17@users.noreply.github.com>
2025-07-25 19:00:47 +0530

    first test case to handle blank value

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,4 +1,8 @@
 Spec.describe Calculator do
   describe "add" do // writing all the possible test cases related to add method
+    it " return 0 if no argument passed" do
+      @calculator = Calculator.new
+      expect(@calculator.add()).to eq(0)
+    end
   end
 end
